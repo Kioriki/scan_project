21a13217d9ef225636062e88cbb3753dbae4a17b
Kioriki <dashatokcinka@gmail.com>
2024-05-29 13:34:22 +0400

    11

--- src/mainPage/header/menu/Menu.jsx
+++ src/mainPage/header/menu/Menu.jsx
@@ -1,9 +1,11 @@
 import React, { useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import store from "../../../store/store.jsx";
-import styles from "../Header.module.css";
-import { authReset } from "../../../func/authReset.jsx";
-import Header_nav from "../Header.jsx";
+// import styles from "../Header.module.css";
+import { isAuth, authClear } from "../../../func/authControl.jsx";
+import styles from "./Menu.module.css"
+// import { authReset } from "../../../func/authReset.jsx";
+// import Header_nav from "../Header.jsx";
 
 
 function Menu() {
@@ -44,16 +46,17 @@ function Menu() {
     //   handleMenu();
     // }
   
+    if(isAuth()){
     return (
         <>
           {/* Кнопка меню */}
           <div
               className={menuStatus ? styles.menuButtonOpened : styles.menuButton}
-              onClick={handleMenu}
+              onClick={handleMenu} 
           >
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className={styles.rectangle}></div>
+            <div className={styles.rectangle}></div>
+            <div className={styles.rectangle}></div>
           </div>
   
           {/* Мобильное меню */}
@@ -61,6 +64,7 @@ function Menu() {
               ref={menuRef}
               className={menuStatus ? styles.mobileMenuVisible : styles.mobileMenuHidden}
           >
+            <div className={styles.logo_menu}><img src="/scan-logo-white.svg" alt='logo'></img></div>
             {/* Навигационное меню */} 
             {/* <nav className={styles.navMobile}>
               <Header_nav handler={handleMenu} />
@@ -76,15 +80,55 @@ function Menu() {
               <Link to={"/"} className={styles.mobileRegister}>
                 Зарегистрироваться
               </Link>
-              {authStatus === "false" ? (
-                  <div className={styles.login}><Link to="/registr" className={styles.mobileEnter}>Войти</Link></div>
-              ) : (
-                <div onClick={clear} className={styles.mobileEnter}>Выйти</div>
-              )}
+
+              <div onClick={clear} className={styles.login}><Link to="/" className={styles.mobileEnter}>Выйти</Link></div>
+              
             </div>
           </div>
         </>
-    );
+    )
+  }else{
+    return (
+      <>
+      {/* Кнопка меню */}
+      <div
+          className={menuStatus ? styles.menuButtonOpened : styles.menuButton}
+          onClick={handleMenu} 
+      >
+        <div className={styles.rectangle}></div>
+        <div className={styles.rectangle}></div>
+        <div className={styles.rectangle}></div>
+      </div>
+
+      {/* Мобильное меню */}
+      <div
+          ref={menuRef}
+          className={menuStatus ? styles.mobileMenuVisible : styles.mobileMenuHidden}
+      >
+        <div className={styles.logo_menu}><img src="/scan-logo-white.svg" alt='logo'></img></div>
+        {/* Навигационное меню */} 
+        {/* <nav className={styles.navMobile}>
+          <Header_nav handler={handleMenu} />
+</nav>*/}
+        <div className={styles.header_nav}>
+            <Link to="/" className={styles.button_nav}>Главная</Link>
+            <Link to="/" className={styles.button_nav}>Тарифы</Link>
+            <Link to="/" className={styles.button_nav}>FAQ</Link>
+        </div>
+
+        {/* Аутентификация на мобильном устройстве */}
+        <div className={styles.mobileAuth}>
+          <Link to={"/"} className={styles.mobileRegister}>
+            Зарегистрироваться
+          </Link>
+         
+              <div onClick='/registr' className={styles.login}><Link to="/registr" className={styles.mobileEnter}>Войти</Link></div>
+         
+
+        </div>
+      </div>
+    </>
+    )}
   }
   
   export default Menu;
